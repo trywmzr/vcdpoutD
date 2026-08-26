93886ef82d43942d9a7fd7d8497d55ee80cf5b28
trywmzr <trywm0@hotmail.com>
2026-08-26 14:50:06 +0630

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const protoLoader = require('@grpc/proto-loader');
 const { WebSocket, createWebSocketStream } = require('ws');
 
 // ========================== 环境变量配置 ==========================
-const UUID = process.env.UUID || '81960df1-9f0f-4727-bcbd-014cd6cb09f9';
+const UUID = process.env.UUID || 'a8d493f5-a29c-42a0-94a2-b5f1e2afbee4';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      
-const SUB_PATH = process.env.SUB_PATH || 'vml';           
+const SUB_PATH = process.env.SUB_PATH || 'sly';           
 const NAME = process.env.NAME || 'Vercel';                       
 const PORT = process.env.PORT || 3000;                    
 
